461b2a700ed6dd71a5e1c20285a41b84b1f8c383
Minsup Kim <minsup@gmail.com>
2013-10-14 01:12:58 -0700

    Initial commit

---
+++ src/com/Minsup/JE4.java
@@ -0,0 +1,42 @@
+package com.Minsup;
+import java.util.*;
+
+public class JE4 {
+	public static void main(String[] args) {
+		// permutation
+		ArrayList<String> str = new ArrayList<String>();
+		str.add("a");
+		str.add("b");
+		str.add("c");
+		str.add("d");
+		str.add("e");
+
+		Permutation(str, 0);
+
+	}
+	
+	static void Permutation(ArrayList<String> str, int index) {
+		int i;
+		if(index == str.size()) {
+			for(String s : str) {
+				System.out.print(s + " ");
+			}
+			System.out.println("");
+			return;
+		}		
+		for(i=index; i<str.size(); i++) {
+			swap(str, i, index);
+			Permutation(str, index+1);
+			swap(str, i, index);
+		}
+
+	}
+	
+	static void swap(ArrayList<String> str, int i, int j) {
+		String temp;
+		
+		temp = str.get(i);
+		str.set(i, str.get(j));
+		str.set(j, temp);
+	}
+}
