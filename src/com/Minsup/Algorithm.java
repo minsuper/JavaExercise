2d9f10427f36ecb40256816378618a340c85e2e5
Minsup Kim <minsup@gmail.com>
2013-10-15 20:09:26 -0700

    Completion of generics example

--- src/com/Minsup/Algorithm.java
+++ src/com/Minsup/Algorithm.java
@@ -3,10 +3,10 @@ package com.Minsup;
 import java.util.ArrayList;
 
 public class Algorithm {
-	static <T> int countOdd(ArrayList<T> c, testOdd<T> t) {
+	public static <T> int countOdd(ArrayList<T> c, testInterface<T> t) {
 		int count = 0;
 		for(T i : c) {
-			if(t.testOdd(i))
+			if(t.test(i))
 				count++;
 		}	
 		return count;
